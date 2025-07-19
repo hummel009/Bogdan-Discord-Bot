167abe1223b14992e836bcdc38e4823ae347372d
Hummel009 <hummel.turbamentum@gmail.com>
2025-07-19 03:09:57 +0300

    Update ControllerImpl.kt

--- appCommon/src/main/kotlin/com/github/hummel/mdb/core/controller/impl/ControllerImpl.kt
+++ appCommon/src/main/kotlin/com/github/hummel/mdb/core/controller/impl/ControllerImpl.kt
@@ -2,10 +2,12 @@ package com.github.hummel.mdb.core.controller.impl
 
 import com.github.hummel.mdb.core.controller.Controller
 import com.github.hummel.mdb.core.factory.ServiceFactory
+import com.github.hummel.mdb.core.service.LoginService
 
 class ControllerImpl : Controller {
+	private val loginService: LoginService = ServiceFactory.loginService
+
 	override fun onCreate() {
-		val loginService = ServiceFactory.loginService
 		loginService.loginBot()
 		//loginService.recreateCommands()
 	}
